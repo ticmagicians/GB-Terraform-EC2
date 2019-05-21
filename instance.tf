ce8b96b3d043fae4355ff7b01481dbd348aa404b
RatanBekal <ratankb@outlook.com>
2019-05-22 00:28:50 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -5,6 +5,7 @@ resource "aws_instance" "web" {
   key_name = "${var.keypair}"
   security_groups = ["${var.security_g}"]
   subnet_id = "${var.subnet_id}"
- 
+  tags {
+    Name = "${var.tag_name}-${count.index + 1}"
   }
 }
