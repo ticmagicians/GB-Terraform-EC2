20c9ef20b757b3350479b4bfe95158ca28c8feaa
RatanBekal <ratankb@outlook.com>
2019-06-02 08:38:07 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
   count = "${var.instance_count}"
-  ami = "ami-09e3977afe9c8920f"
+  ami = "ami-004f66641f4c4c798"
   instance_type = "t2.micro"
   key_name = "${var.keypair}"
   security_groups = ["${var.security_g}"]
