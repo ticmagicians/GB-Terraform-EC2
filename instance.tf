1c844839c26f2d8399e5c3e41e8487660837f267
ticmagicians <51209190+ticmagicians@users.noreply.github.com>
2019-06-15 06:53:40 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "web" {
   security_groups = ["${var.security_g}"]
   subnet_id = "${var.subnet_id}"
   tags = {
-    Name = "${var.tag_name} ${count.index + 1}"
+    Name = "${var.tag_name}${count.index + 1}"
   }
 }
 
