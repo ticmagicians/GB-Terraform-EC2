5ea539529205930a014aee839fb495626abaab3f
ticmagicians <51209190+ticmagicians@users.noreply.github.com>
2019-06-15 06:34:28 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -5,10 +5,7 @@ resource "aws_instance" "web" {
   key_name = "${var.keypair}"
   security_groups = ["${var.security_g}"]
   subnet_id = "${var.subnet_id}"
-  tags { 
-    Name = "${var.tag_name} ${count.index + 1}" 
-  } 
-
+  tags = "${var.tag_name} ${count.index + 1}" 
 }
 
 #Instance Attachment
